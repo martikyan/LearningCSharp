cc5fae1a3db4cd8774594f168f5ad5e7252ce1bf
Karen Martikyan <kamartikyan@gmail.com>
2019-05-05 20:51:49 +0400

    Delegate trick implemented

---
+++ Lessons/Delegates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningCSharp.Lessons
+{
+    public class Delegates
+    {
+        private delegate void Printer();
+
+        [Demo]
+        public static void DelegateTrick()
+        {
+            List<Printer> printers = new List<Printer>(); // Printer or Action
+            int i = 0;
+            for (; i < 10; i++)
+            {
+                printers.Add(delegate { Console.WriteLine(i); });
+            }
+
+            foreach (var printer in printers)
+            {
+                printer(); // 10 times the number 10
+            }
+        }
+    }
+}
