0a48873f9bd2fb0108695c6c313ed7fe2e2e86f7
Karen Martikyan <kamartikyan@gmail.com>
2019-05-09 23:00:51 +0400

    Small improvements to delegates

--- Lessons/Delegates.cs
+++ Lessons/Delegates.cs
@@ -12,14 +12,32 @@ namespace LearningCSharp.Lessons
         {
             List<Printer> printers = new List<Printer>(); // Printer or Action
             int i = 0;
-            for (; i < 10; i++)
+            for (; i < 3; i++)
             {
                 printers.Add(delegate { Console.WriteLine(i); });
             }
 
             foreach (var printer in printers)
             {
-                printer(); // 10 times the number 10
+                printer(); // 3 times the number 3
+            }
+        }
+
+        [Demo]
+        public static void DelegateTrickOvercome()
+        {
+            var printers = new List<Printer>();
+            var i = 0;
+
+            for (; i < 3; i++)
+            {
+                int _i = i; // Copy of the i;
+                printers.Add(delegate { Console.WriteLine(_i); });
+            }
+
+            foreach (var printer in printers)
+            {
+                printer(); // 0, 1, 2
             }
         }
     }
