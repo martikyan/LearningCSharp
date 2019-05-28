b3b011dd051e7479524b9edb898c4c63156ae843
Karen Martikyan <karen@concent.io>
2019-05-28 13:01:53 +0400

    Minor fixes

--- Lessons/Variances.cs
+++ Lessons/Variances.cs
@@ -9,42 +9,42 @@ namespace LearningCSharp.Lessons
         public static void ArrayTypeMismatch()
         {
             Terrier[] terriers = new Terrier[3];
-            Dog[] dogs = (Dog[])terriers;
-            dogs[0] = new Pitbull();
+            Dog[] dogs = terriers;
+            dogs[0] = new Pitbull(); // Exception here
             Console.WriteLine(dogs[0]);
         }
 
         [Demo]
         public static void Covariance()
         {
-            // Assignment compatibility.   
+            // Assignment compatibility.
             string str = "test";
-            // An object of a more derived type is assigned to an object of a less derived type.   
+            // An object of a more derived type is assigned to an object of a less derived type.
             object obj = str;
 
-            // Covariance.   
+            // Covariance.
             IEnumerable<string> strings = new List<string>();
-            // An object that is instantiated with a more derived type argument   
-            // is assigned to an object instantiated with a less derived type argument.   
-            // Assignment compatibility is preserved.   
+            // An object that is instantiated with a more derived type argument
+            // is assigned to an object instantiated with a less derived type argument.
+            // Assignment compatibility is preserved.
             IEnumerable<object> objects = strings;
         }
 
         [Demo]
         public static void Contravariance()
         {
-            // Assignment compatibility.   
+            // Assignment compatibility.
             string str = "test";
-            // An object of a more derived type is assigned to an object of a less derived type.   
+            // An object of a more derived type is assigned to an object of a less derived type.
             object obj = str;
 
-            // Contravariance.             
-            // Assume that the following method is in the class:   
-            // static void SetObject(object o) { }   
+            // Contravariance.
+            // Assume that the following method is in the class:
+            // static void SetObject(object o) { }
             Action<object> actObject = (object o) => { };
-            // An object that is instantiated with a less derived type argument   
-            // is assigned to an object instantiated with a more derived type argument.   
-            // Assignment compatibility is reversed.   
+            // An object that is instantiated with a less derived type argument
+            // is assigned to an object instantiated with a more derived type argument.
+            // Assignment compatibility is reversed.
             Action<string> actString = actObject;
         }
 
